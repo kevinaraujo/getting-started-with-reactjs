b054cecaa12cd2f2addb842601a05a68a1db97aa
Kevin Araújo <kfa_34@hotmail.com>
2020-05-08 20:01:58 -0300

    hello world

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <div className="my-class-from-react">
+                <h1>Hello World React By School of net</h1>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld
